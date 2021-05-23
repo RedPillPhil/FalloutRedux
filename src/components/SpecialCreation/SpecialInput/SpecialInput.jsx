d6df2ab7865c5a492eb0934385e02358654b94d7
vladilen597 <vladilen597@gmail.com>
2021-05-23 21:01:50 +0300

    added pics

--- src/components/SpecialCreation/SpecialInput/SpecialInput.jsx
+++ src/components/SpecialCreation/SpecialInput/SpecialInput.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./SpecialInput.css";
-import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const SpecialInput = (props) => {
   return (
